6b29b06e7df44eaa3005c0ec5cc7277b56cf27b3
juetan <contact@juetan.cn>
2023-07-24 21:09:20 +0800

    feat: 添加GitTalk评论系统

--- .vitepress/theme/override/Layout.vue
+++ .vitepress/theme/override/Layout.vue
@@ -11,24 +11,52 @@
         <Button type="dashed" size="large" shape="circle" class="hidden md:inline-block">UP</Button>
       </BackTop>
     </template>
+    <template #doc-bottom>
+      <div id="gittalk"></div>
+    </template>
   </Layout>
 </template>
 
 <script setup lang="ts">
 import { BackTop, Button } from '@arco-design/web-vue';
+import { fairyDustCursor } from 'cursor-effects';
+import Gitalk from 'gitalk';
+import 'gitalk/dist/gitalk.css';
+import { useRoute } from 'vitepress';
 import theme from 'vitepress/theme';
+import { onMounted, watch } from 'vue';
 import DocNotFound from '../components/DocNotFound.vue';
 import DocTitle from '../components/DocTitle.vue';
-import { onMounted } from 'vue';
-import { fairyDustCursor } from 'cursor-effects';
 
 const { Layout } = theme;
+
+const initGitTalk = () => {
+  const gitalk = new Gitalk({
+    clientID: '8786fc7940415da9145f',
+    clientSecret: 'd22ea694891436d822e392611b536d9df0612ce7',
+    owner: 'juetan',
+    repo: 'juetan.github.io',
+    id: encodeURIComponent(location.pathname),
+    admin: ['juetan'],
+  });
+  gitalk.render('gittalk');
+};
 
 onMounted(() => {
   new fairyDustCursor({
     colors: ['#98EBC7', '#89E9E0', '#9FD4FD', '#C396ED', '#F08EE6', '#FCC59F', '#FFCF8B'],
   });
+  initGitTalk();
 });
+
+const route = useRoute();
+
+watch(
+  () => route.path,
+  () => {
+    initGitTalk();
+  }
+);
 </script>
 
 <style scoped></style>
