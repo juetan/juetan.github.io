d23e3bcd83bf74139c49cc9c8e7333633e6f8908
juetan <contact@juetan.cn>
2023-08-08 21:34:33 +0800

    feat: 优化评论样式

--- .vitepress/theme/components/DocTitle.vue
+++ .vitepress/theme/components/DocTitle.vue
@@ -3,7 +3,7 @@
     <h1 class="doc-title">
       {{ frontmatter.title }}
     </h1>
-    <div class="mt-1.5 text-gray-400 text-xs flex items-center gap-3 border-b border-[var(--vp-c-divider)] pb-3 mb-6">
+    <div class="mt-1.5 text-gray-400 text-xs flex items-center gap-3 border-b border-[var(--vp-c-divider)] pb-4 mb-6">
       <div class="cursor-pointer hover:text-[var(--vp-c-brand)]">
         <span class="i-icon-park-outline-user" />
         绝弹
@@ -25,9 +25,9 @@
 </template>
 
 <script setup lang="ts">
+import { PostItem, data } from '@app/data/posts.data.ts';
 import { useData } from 'vitepress';
 import { dayjs } from '../dayjs';
-import { PostItem, data } from '@app/data/posts.data.ts';
 
 const { frontmatter, page } = useData<PostItem>();
 const item = data.find((i) => i.frontmatter.title === page.value.frontmatter.title);
