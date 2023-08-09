f2e9caeba4215eb179c5ba7b0b9054722820d71d
luoer <810335188@qq.com>
2023-08-09 11:00:10 +0800

    feat: 优化备案号显示

--- .vitepress/theme/override/Layout.vue
+++ .vitepress/theme/override/Layout.vue
@@ -7,7 +7,6 @@
       <DocNotFound></DocNotFound>
     </template>
     <template #layout-bottom>
-      <a href="https://beian.miit.gov.cn/" target="_blank">桂ICP备2021001859号</a>
       <BackTop>
         <Button type="primary" class="!hidden !md:inline-block">
           <template #icon>
@@ -29,24 +28,14 @@
 import { BackTop, Button } from '@arco-design/web-vue';
 import { Waline } from '@waline/client/component';
 import '@waline/client/dist/waline.css';
-import { fairyDustCursor } from 'cursor-effects';
 import { useRoute } from 'vitepress';
 import theme from 'vitepress/theme';
-import { nextTick, onMounted, onUnmounted, watch } from 'vue';
 import DocNotFound from '../components/DocNotFound.vue';
 import DocTitle from '../components/DocTitle.vue';
-import { useHeadScroll } from './state';
 
 const { Layout } = theme;
 const route = useRoute();
 const serverURL = 'https://jtwaline.vercel.app';
-const toc = useHeadScroll();
-
-onMounted(() => {
-  // new fairyDustCursor({
-  //   colors: ['#98EBC7', '#89E9E0', '#9FD4FD', '#C396ED', '#F08EE6', '#FCC59F', '#FFCF8B'],
-  // });
-});
 </script>
 
 <style scoped></style>
